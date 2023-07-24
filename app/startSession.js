8570671811096f85d25a195b5de9096e4daf0182
Angie <angelicazamudiocamargo@gmail.com>
2023-07-23 19:47:40 -0600

    trying to work on dom with typescript

---
+++ app/startSession.js
@@ -0,0 +1,8 @@
+var loginSection = document.getElementById('login-section');
+console.log(loginSection);
+// const loginButton = loginSection.querySelector('button');
+// console.log(loginButton);
+// loginButton.addEventListener('click', cambiarPagina);
+// function cambiarPagina(){
+//     window.location.href ='../index.html';
+// }
---
+++ app/startSession.ts
@@ -0,0 +1,13 @@
+let loginSection: object | null = document.getElementById('login-section');
+
+console.log(loginSection);
+
+
+
+
+// const loginButton = loginSection.querySelector('button');
+// console.log(loginButton);
+// loginButton.addEventListener('click', cambiarPagina);
+// function cambiarPagina(){
+//     window.location.href ='../index.html';
+// }
